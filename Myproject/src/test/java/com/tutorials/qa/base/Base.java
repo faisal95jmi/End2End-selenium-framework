805cf8a93cc4d3fbb07bc0018fc01d3b7525e748
faisal <mohammadfaisalk10@gmail.com>
2023-01-17 18:13:55 +0530

    selenium with testng

---
+++ Myproject/src/test/java/com/tutorials/qa/base/Base.java
@@ -0,0 +1,83 @@
+package com.tutorials.qa.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.tutorials.qa.utils.Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+	
+	WebDriver driver;
+	public Properties prop;
+	public Properties dataProp;
+		
+	public Base() {
+		
+		prop = new Properties();
+		File propFile = new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\tutorials\\qa\\config\\config.properties");
+		
+		dataProp = new Properties();
+		File dataPropFile = new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\tutorials\\qa\\testdata\\testdata.properties");
+		
+		try {
+			FileInputStream dataFis = new FileInputStream(dataPropFile);
+			dataProp.load(dataFis);
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			FileInputStream fis = new FileInputStream(propFile);
+			prop.load(fis);
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public WebDriver initializeBrowserAndOpenApplicationURL(String browserName) {
+			
+		if(browserName.equalsIgnoreCase("chrome")) {
+			
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		}else if(browserName.equalsIgnoreCase("firefox")) {
+			
+			driver = new FirefoxDriver();
+			
+		}else if(browserName.equalsIgnoreCase("edge")) {
+			
+			driver = new EdgeDriver();
+			
+		}else if(browserName.equalsIgnoreCase("safari")) {
+			
+			driver = new SafariDriver();
+			
+		}
+	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.IMPLICIT_WAIT_TIME));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Utilities.PAGE_LOAD_TIME));
+		driver.get(prop.getProperty("url"));
+		
+		return driver;
+		
+	}
+	
+	
+	
+	
+	
+}
